0a1373bbc65a7fb1bbe0c9c314854a92feef520a
AntonShevchuk <AntonShevchuk@gmail.com>
2013-11-26 15:18:02 +0200

    Updated manifest

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Extension for Chrome
 ====================
 
-Browser extension for Bluz developers
+Browser extension for Bluz developers.
+Available on [Google Store](https://chrome.google.com/webstore/detail/bluz-toolbar/ldlhdffneodlnkhggbpgnlmhphniaibn/related?hl=ru&gl=UA)
--- js/options.js
+++ js/options.js
@@ -1,45 +1,44 @@
-    var COOKIE = {
-        debug   : 'BLUZ_DEBUG',
-        profile : 'XDEBUG_PROFILE',
-        headers : 'Bluz-Bar, Bluz-Notify',
-        header_bar: 'Bluz-Debug'
-    }
+var COOKIE = {
+    debug   : 'BLUZ_DEBUG',
+    profile : 'XDEBUG_PROFILE',
+    headers : 'Bluz-Bar, Bluz-Notify',
+    header_bar: 'Bluz-Debug'
+};
 
-    function get(id){
-        return document.getElementById(id);
-    }
+function get(id){
+    return document.getElementById(id);
+}
 
-    function save_options() {
-        localStorage["cookie_debug"] = get("cookie_debug").value;
-        localStorage["cookie_profile"] = get("cookie_profile").value;
-        localStorage["headers_details"] = get("headers_details").value;
-        localStorage["header_bar"] = get("header_bar").value;
+function save_options() {
+    localStorage["cookie_debug"] = get("cookie_debug").value;
+    localStorage["cookie_profile"] = get("cookie_profile").value;
+    localStorage["headers_details"] = get("headers_details").value;
+    localStorage["header_bar"] = get("header_bar").value;
 
-        // Update status to let user know options were saved.
-        var status = get("status");
-        status.innerHTML = "Options Saved.";
-        setTimeout(function() {
-            status.innerHTML = "";
-        }, 750);
-    }
+    // Update status to let user know options were saved.
+    var status = get("status");
+    status.innerHTML = "Options Saved.";
+    setTimeout(function() {
+        status.innerHTML = "";
+    }, 750);
+}
 
 
-    function restore_options() {
-        get("cookie_debug").setAttribute('value', localStorage["cookie_debug"] || COOKIE.debug);
-        get("cookie_profile").setAttribute('value', localStorage["cookie_profile"] || COOKIE.profile);
-        get("headers_details").setAttribute('value', localStorage["headers_details"] || COOKIE.headers);
-        get("header_bar").setAttribute('value', localStorage["header_bar"] || COOKIE.header_bar);
-    }
+function restore_options() {
+    get("cookie_debug").setAttribute('value', localStorage["cookie_debug"] || COOKIE.debug);
+    get("cookie_profile").setAttribute('value', localStorage["cookie_profile"] || COOKIE.profile);
+    get("headers_details").setAttribute('value', localStorage["headers_details"] || COOKIE.headers);
+    get("header_bar").setAttribute('value', localStorage["header_bar"] || COOKIE.header_bar);
+}
 
-    function reset_options() {
-        localStorage["cookie_debug"] = COOKIE.debug;
-        localStorage["cookie_profile"] = COOKIE.profile;
-        localStorage["headers_details"] = COOKIE.headers;
-        localStorage["header_bar"] = COOKIE.header_bar;
+function reset_options() {
+    localStorage["cookie_debug"] = COOKIE.debug;
+    localStorage["cookie_profile"] = COOKIE.profile;
+    localStorage["headers_details"] = COOKIE.headers;
+    localStorage["header_bar"] = COOKIE.header_bar;
+    restore_options();
+}
 
-        restore_options();
-    }
-
-    document.addEventListener('DOMContentLoaded', restore_options);
-    document.querySelector('#btn-save').addEventListener('click', save_options);
-    document.querySelector('#btn-reset').addEventListener('click', reset_options);
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('btn-save').addEventListener('click', save_options);
+document.getElementById('btn-reset').addEventListener('click', reset_options);
